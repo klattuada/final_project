b2be2e430459807a9ceb38ee46a22afe0ba2004d
pmajdalani <90423821+pmajdalani@users.noreply.github.com>
2021-12-06 18:34:26 -0500

    names and bitmojis
    added the names and bitmojis to the home screen

--- plinko.js
+++ plinko.js
@@ -1,3 +1,119 @@
+//BITMOJI CHARACTERS
+var drawBitmojiHead = function(bitX,bitY,bitHeight) {
+    
+    //head
+    fill(198, 134, 69);
+    ellipse(bitX-(bitHeight/100*40),bitY-(bitHeight/100*50),(bitHeight/100*42),(bitHeight/100*42));//shape
+    stroke(0, 0, 0);
+    arc(bitX-(bitHeight/100*40),bitY-(bitHeight/100*40),(bitHeight/100*25),(bitHeight/100*25),24,151);//chin
+    fill(0, 0, 0);
+    //headgear
+    fill(0, 0, 0);
+    arc(bitX-(bitHeight/100*58),bitY-(bitHeight/100*50),(bitHeight/100*25),(bitHeight/100*25),93,263);//leftside
+    arc(bitX-(bitHeight/100*21),bitY-(bitHeight/100*50),(bitHeight/100*25),(bitHeight/100*25),273,450);//rightside
+    rect(bitX-(bitHeight/100*42),bitY-(bitHeight/100*31),(bitHeight/100*5),(bitHeight/100*3));//chinstrap
+    //hair
+    arc(bitX-(bitHeight/100*41),bitY-(bitHeight/100*68),(bitHeight/100*28),(bitHeight/100*9),159,368);//top
+    arc(bitX-(bitHeight/100*55),bitY-(bitHeight/100*63),(bitHeight/100*8),(bitHeight/100*9),141,305);//leftside
+    arc(bitX-(bitHeight/100*25),bitY-(bitHeight/100*64),(bitHeight/100*12),(bitHeight/100*7),243,428);//rightside
+    //face
+    fill(255, 255, 255);//eye color
+    ellipse(bitX-(bitHeight/100*46),bitY-(bitHeight/100*53),(bitHeight/100*9),(bitHeight/100*8));//left eye
+    fill(99, 69, 12);
+    ellipse(bitX-(bitHeight/100*46),bitY-(bitHeight/100*51),(bitHeight/100*2),(bitHeight/100*2));//pupill
+    fill(255,255,255);
+    ellipse(bitX-(bitHeight/100*32),bitY-(bitHeight/100*53),(bitHeight/100*9),(bitHeight/100*8));//right eye
+    fill(99, 69, 12);
+    ellipse(bitX-(bitHeight/100*32),bitY-(bitHeight/100*51),(bitHeight/100*2),(bitHeight/100*2));//pupill
+    fill(0, 0, 0);
+    arc(bitX-(bitHeight/100*48),bitY-(bitHeight/100*59),(bitHeight/100*13),(bitHeight/100*4),237,394);//eyebrow
+    arc(bitX-(bitHeight/100*29),bitY-(bitHeight/100*59),(bitHeight/100*13),(bitHeight/100*4),172,307);//eyebrow
+    noFill();
+    fill(198, 134, 69);
+    ellipse(bitX-(bitHeight/100*39),bitY-(bitHeight/100*46),(bitHeight/100*6),(bitHeight/100*8));//nose
+    ellipse(bitX-(bitHeight/100*39),bitY-(bitHeight/100*43),(bitHeight/100*7),(bitHeight/100*5));//top of nose
+    noFill();
+    ellipse(bitX-(bitHeight/100*26),bitY-(bitHeight/100*46),(bitHeight/100*1),(bitHeight/100*0));//dot
+    stroke(156, 0, 83);
+    fill(0, 0, 0);
+    ellipse(bitX-(bitHeight/100*39),bitY-(bitHeight/100*36),(bitHeight/100*10),(bitHeight/100*4));//mouth
+
+};
+var drawBitmojiBody = function (bitX,bitY,bitHeight) {
+    //shirt
+    fill(0,0,0);
+    stroke(0, 0, 0);
+    quad(bitX-(bitHeight/100*40),bitY-(bitHeight/100*27),bitX-(bitHeight/100*41),bitY-(bitHeight/100*3),bitX-(bitHeight/100*7),bitY-(bitHeight/100*5),bitX-(bitHeight/100*15),bitY-(bitHeight/100*27));
+quad(bitX-(bitHeight/100*38),bitY-(bitHeight/100*27),bitX-(bitHeight/100*39),bitY-(bitHeight/100*3),bitX-(bitHeight/100*66),bitY-(bitHeight/100*5),bitX-(bitHeight/100*63),bitY-(bitHeight/100*26));
+    fill(250, 250, 250);
+    textSize((bitHeight/100*10));
+    text("KL",bitX-(bitHeight/100*45),bitY-(bitHeight/100*10));
+
+};
+var drawBitmoji = function(bitX,bitY,bitHeight) {
+    
+    drawBitmojiHead(bitX,bitY,bitHeight);
+    drawBitmojiBody(bitX,bitY,bitHeight);
+};
+var drawHead = function(bitmojiX,bitmojiY,resize){
+    noStroke();
+    fill(255, 219, 172); // skin tone for head
+    ellipse(bitmojiX,bitmojiY,resize/100*94,resize/100*111); //head
+    fill(255, 255, 255); // white for eye
+    stroke(0, 0, 0);
+    ellipse(resize/100*-16+bitmojiX,resize/100*-20+bitmojiY,resize/100*14,resize/100*14); // left eye
+    ellipse(resize/100*17+bitmojiX,resize/100*-20+bitmojiY,resize/100*14,resize/100*14); // right eye
+    noStroke();
+    fill(82, 54, 6); // brown for pupil
+    ellipse(resize/100*-16+bitmojiX,resize/100*-20+bitmojiY,resize/100*6,resize/100*6); //left pupil
+    ellipse(resize/100*17+bitmojiX,resize/100*-20+bitmojiY,resize/100*6,resize/100*6); // right pupil
+    stroke(0, 0, 0);
+    fill(255, 219, 172); // skin tone for ears
+    arc(resize/100*-45+bitmojiX, resize/100*-0+bitmojiY, resize/100*21, resize/100*27, 88, 270); // left ear
+    arc(resize/100*45+bitmojiX, resize/100*0+bitmojiY, resize/100*21, resize/100*27, -90, 92); // right ear
+    noStroke();
+    stroke(0, 0, 0); // line for nose 
+    fill(255, 219, 172); // skin tone for nose
+    triangle(resize/100*9+bitmojiX,resize/100*8+bitmojiY,resize/100*0+bitmojiX,resize/100*-20+bitmojiY,resize/100*-7+bitmojiX,resize/100*8+bitmojiY); // nose
+    noStroke();
+    fill(28, 19, 0); //color for sideburns
+    rect(resize/100*-45+bitmojiX,resize/100*-33+bitmojiY,resize/100*8,resize/100*46); // left sideburn
+    rect(resize/100*39+bitmojiX,resize/100*-33+bitmojiY,resize/100*8,resize/100*46); // right sideburn
+    fill(10, 10, 10); // color for hat
+    arc(resize/100*1+bitmojiX, resize/100*-30+bitmojiY, resize/100*93, resize/100*101, 184, 358); // hat
+    rect(resize/100*-39+bitmojiX,resize/100*-46+bitmojiY,resize/100*118,resize/100*15); // brim of hat
+    fill(255, 255, 255); //color for hat design
+    rect(resize/100*-5+bitmojiX,resize/100*-77+bitmojiY,resize/100*3,resize/100*12); // hat design #1
+    rect(resize/100*1+bitmojiX,resize/100*-77+bitmojiY,resize/100*3,resize/100*12); // hat design #2
+    rect(resize/100*9+bitmojiX,resize/100*-61+bitmojiY,resize/100*12,resize/100*3); // hat design #3
+    rect(resize/100*9+bitmojiX,resize/100*-55+bitmojiY,resize/100*12,resize/100*3); // hat design #4
+    rect(resize/100*1+bitmojiX,resize/100*-49+bitmojiY,resize/100*3,resize/100*12); // hat design #5
+    rect(resize/100*-5+bitmojiX,resize/100*-49+bitmojiY,resize/100*3,resize/100*12); // hat design #6
+    rect(resize/100*-21+bitmojiX,resize/100*-61+bitmojiY,resize/100*12,resize/100*3); // hat design #7
+    rect(resize/100*-21+bitmojiX,resize/100*-55+bitmojiY,resize/100*12,resize/100*3); // hat design #8
+    fill(255, 219, 172);
+    rect(resize/100*-19+bitmojiX,resize/100*53+bitmojiY,resize/100*38,resize/100*44); // neck
+    fill(28, 19, 0); // color for beard
+    arc(resize/100*1+bitmojiX, resize/100*13+bitmojiY, resize/100*92, resize/100*95, 1, 180); // beard
+    fill(255, 250, 250); // white for mouth
+    arc(resize/100*0+bitmojiX, resize/100*21+bitmojiY, resize/100*50, resize/100*51, 1, 180); // mouth
+    stroke(0, 0, 0); //black for line
+    line(resize/100*-22+bitmojiX, resize/100*32+bitmojiY, resize/100*21+bitmojiX, resize/100*32+bitmojiY); // teeth #1
+    line(resize/100*-14+bitmojiX, resize/100*43+bitmojiY, resize/100*-14+bitmojiX, resize/100*20+bitmojiY); // teeth #2
+    line(resize/100*0+bitmojiX, resize/100*46+bitmojiY, resize/100*0+bitmojiX, resize/100*20+bitmojiY); // teeth #3
+    line(resize/100*12+bitmojiX, resize/100*47+bitmojiY, resize/100*12+bitmojiX, resize/100*20+bitmojiY); // teeth #4
+};
+var drawBody = function(bitmojiX,bitmojiY,resize){
+    noStroke();
+    fill(230, 7, 7); // color for shirt
+    rect(resize/100*-85+bitmojiX,resize/100*78+bitmojiY,resize/100*174,resize/100*157,40); // shirt
+};
+var drawBitmojipeter = function(bitmojiX,bitmojiY,resize){
+    drawHead(bitmojiX,bitmojiY,resize);
+    drawBody(bitmojiX,bitmojiY,resize);
+};
+
+
 //added colors 12/5
 var gravity = 0.06; // speed at which the ball drops  ideal speed = 0.06
 var pegs = []; //empty array for pegs
@@ -60,6 +176,7 @@ Pegs.prototype.draw = function() {
     ellipse(this.x,this.y,this.width,this.height);
 };
 
+//Slot constuctor function
 var Slot = function(position){
     this.position = position;
     this.x = 56*position;
